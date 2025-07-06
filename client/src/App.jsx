a3bcd225c0ac7454d6604df86afc95171f861c13
Mainak-Das <mainakd545@gmail.com>
2025-07-07 02:12:55 +0530

    Cart Page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import Home from './pages/Home'
-import AllProducts from './pages/AllProducts'
 import { ToastBar, Toaster } from 'react-hot-toast'
 import Footer from './components/Footer'
 import { useAppContext } from './context/AppContext'
 import Login from './components/Login'
+import Home from './pages/Home'
+import AllProducts from './pages/AllProducts'
 import ProductCategory from './pages/ProductCategory'
 import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
 
 
 
@@ -30,6 +31,7 @@ const App = () => {
           <Route path='/products' element={<AllProducts/>} />
           <Route path='/products/:category' element={<ProductCategory />} />
           <Route path='/products/:category/:id' element={<ProductDetails />} />
+          <Route path='/cart' element={<Cart />} />
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
